c60d0a9dcda5a9054c9e08a0dac82ee64d868bc7
Michael Davis <michael.davis@ksuwdc.com>
2014-02-17 21:23:14 -0500

    cucpcake

--- parts/shared/header.php
+++ parts/shared/header.php
@@ -2,4 +2,5 @@
 	<h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
 	<?php bloginfo( 'description' ); ?>
 	<?php get_search_form(); ?>
+	<img src="/wp-content/themes/starkers/images/ccake.png"/>
 </header>
